a3006fe0ddd4fd3a5e3ceca3cc3ae4539e5f503e
M <monacuaulma@gmail.com>
2024-02-23 00:04:37 -0400

    Agregando los recursos

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primaryGreen: "#03b044",
+				customRed: "#e92100",
+				hoverGreen: "#07913b",
+				darkGreen: "#025923",
+				lightGrey: "#f7f8fb",
+				darkWhite: "#f5f6fa",
+			},
+		},
+		screens: {
+			sm: { max: "899px" },
+			md: "900px",
+		},
 	},
 	plugins: [],
 };
